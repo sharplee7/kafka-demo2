4edc32d5cae7a79d64771ef302d3f27f67d60f25
이의덕 책임 MSA모델링팀 (euideok.lee@lgcns.com <euideok.lee@CNS.LGCNS.COM>
2021-08-27 15:59:48 +0900

    demo up grade

--- kafka-pub/src/main/java/com/example/pub/controller/PublisherController.java
+++ kafka-pub/src/main/java/com/example/pub/controller/PublisherController.java
@@ -3,10 +3,16 @@ package com.example.pub.controller;
 
 import com.example.common.EmpDTO;
 import com.example.pub.service.EmpService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,13 +22,20 @@ public class PublisherController {
     @Autowired
     EmpService empService;
 
+    @Operation(summary = "사용자 저장", description = "사용자 정보를 저장합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @PostMapping(value = "/employee")
     public ResponseEntity save(@RequestBody final EmpDTO empDTO) throws Exception {
-        if(empService.insertEmp(empDTO) == 1) {
-            return new ResponseEntity<>("success", HttpStatus.OK);
-        } else {
+        if(empService.insertEmp(empDTO) != 1) {
             return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
         }
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
     @GetMapping(value = "/employee")

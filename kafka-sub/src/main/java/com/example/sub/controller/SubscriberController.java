65b2c9ff7f1a3e6bf0d3bf4f83633d39677525ba
euideok.lee <sharplee@gmail.com>
2021-08-29 23:15:48 +0900

    modify mongodb

--- kafka-sub/src/main/java/com/example/sub/controller/SubscriberController.java
+++ kafka-sub/src/main/java/com/example/sub/controller/SubscriberController.java
@@ -5,8 +5,6 @@ import com.example.common.EmpDTO;
 import com.example.sub.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
